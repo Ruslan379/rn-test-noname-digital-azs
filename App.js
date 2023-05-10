06d08b536d74b5b2ae9f535d898d8936a0350a58
Ruslan379 <arnov@i.ua>
2023-05-10 15:49:14 +0300

    rn-2.12

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import {
   StyleSheet,
@@ -7,38 +8,90 @@ import {
   TextInput,
   // Button,
   TouchableOpacity,
+  Platform,
+  // KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
 } from 'react-native';
 
+// -------------------------------------------------------------------------------------------------------
+const initialState = {
+  // nickname: "",
+  email: "",
+  password: ""
+}
 
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 export default function App() {
+  //! useState
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  const [state, setState] = useState(initialState);
+
+  //! Закрытие клавиатуры
+  const keboardHide = () => {
+    setIsShowKeyboard(false);
+    // setIsFocusedLogin(false);
+    // setIsFocusedMail(false);
+    // setIsFocusedPassword(false);
+    Keyboard.dismiss();
+    console.log("state:", state);
+    setState(initialState);
+  };
+
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={require('./assets/images/background.jpg')}
-        style={styles.image}>
-        <View style={styles.innerBox}>
-
-          <Text style={styles.textOne}>Open up App</Text>
-          <Text style={styles.textTwo}>Open up App.js to start working on your app!</Text>
-        </View>
-        <View style={styles.form}>
-          <View>
-            <Text style={styles.inputTitle}>EMAIL</Text>
-            <TextInput style={styles.input} />
+    <TouchableWithoutFeedback onPress={keboardHide}>
+      <View style={styles.container}>
+        <ImageBackground
+          source={require('./assets/images/background.jpg')}
+          style={styles.image}
+        >
+          {/* <KeyboardAvoidingView
+          behavior={Platform.OS === 'android' ? 'padding ' : 'height'}
+        // style={styles.containerKeyboardAvoidingView}
+        > */}
+          <View style={styles.innerBox}>
+            <Text style={styles.textOne}>Open up App</Text>
+            <Text style={styles.textTwo}>Open up App.js to start working on your app!</Text>
           </View>
-          <View style={{ marginTop: 20 }}>
-            <Text style={styles.inputTitle}>PASSWORD</Text>
-            <TextInput style={styles.input} secureTextEntry={true} />
+          <View style={{ ...styles.form, marginBottom: isShowKeyboard ? 20 : 200 }}
+          >
+            <View style={styles.header}>
+              <Text style={styles.headerTitle}>HEADER-1</Text>
+              <Text style={styles.headerTitle}>HEADER-2</Text>
+            </View>
+            <View>
+              <Text style={styles.inputTitle}>EMAIL</Text>
+              <TextInput
+                style={styles.input}
+                onFocus={() => setIsShowKeyboard(true)}
+                value={state.email}
+                onChangeText={(value) => setState((prevState) => ({ ...prevState, email: value }))}
+              />
+            </View>
+            <View style={{ marginTop: 20 }}>
+              <Text style={styles.inputTitle}>PASSWORD</Text>
+              <TextInput
+                style={styles.input}
+                onFocus={() => setIsShowKeyboard(true)}
+                value={state.password}
+                onChangeText={(value) => setState((prevState) => ({ ...prevState, password: value }))}
+                secureTextEntry={true}
+              />
+            </View>
+            {/* <Button title='SIGN IN' /> */}
+            <TouchableOpacity
+              style={styles.btn}
+              activeOpacity={0.7}
+              onPress={keboardHide}
+            >
+              <Text style={styles.btnTitle}>SIGN IN</Text>
+            </TouchableOpacity>
           </View>
-          {/* <Button title='SIGN IN' /> */}
-          <TouchableOpacity activeOpacity={0.7} style={styles.btn}>
-            <Text style={styles.btnTitle}>SIGN IN</Text>
-          </TouchableOpacity>
-        </View>
-
-      </ImageBackground>
-      <StatusBar style="auto" />
-    </View>
+          {/* </KeyboardAvoidingView> */}
+        </ImageBackground>
+        <StatusBar style="auto" />
+      </View >
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -54,7 +107,11 @@ const styles = StyleSheet.create({
     resizeMode: "cover",
     marginHorizontal: 20,
     // alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
+    justifyContent: 'flex-end',
+  },
+  containerKeyboardAvoidingView: {
+    flex: 1,
   },
   innerBox: {
     // flex: 1,
@@ -78,6 +135,7 @@ const styles = StyleSheet.create({
   },
   form: {
     marginHorizontal: 20,
+    // marginBottom: 100,
   },
   inputTitle: {
     color: "darkmagenta",
@@ -97,16 +155,38 @@ const styles = StyleSheet.create({
   btn: {
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: "darkorange",
+    borderWidth: 2,
     borderRadius: 5,
     // width: 100,
     height: 40,
     marginTop: 20,
     marginHorizontal: 20,
-
+    // borderColor: Platform.OS === "ios" ? "darkorange" : "deepskyblue",
+    // backgroundColor: Platform.OS === "ios" ? "deepskyblue" : "darkorange",
+    ...Platform.select({
+      ios: {
+        borderColor: "darkorange",
+        backgroundColor: 'deepskyblue',
+      },
+      android: {
+        borderColor: "deepskyblue",
+        backgroundColor: 'darkorange',
+      },
+    }),
   },
   btnTitle: {
     color: "darkolivegreen",
     fontSize: 16,
+  },
+  header: {
+    // fontSize: 20,
+    // color: "deeppink ",
+    // textAlign: 'center',
+    alignItems: 'center'
+  },
+  headerTitle: {
+    fontSize: 20,
+    color: "deeppink",
+    // textAlign: 'center',
   },
 });
